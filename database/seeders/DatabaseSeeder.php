f7806cf448a4a268bca7b5b9dc2ef674f3cc36f8
bambangsy <bambangsy28@gmail.com>
2024-06-03 14:57:45 +0700

    add more

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +16,38 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $adminRole = Role::create(['name' => 'admin']);
+        $userRole = Role::create(['name' => 'user']);
+        $agentRole = Role::create(['name' => 'agent']);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Creating a new user
+        $user = User::create([
+            'name' => 'New User',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('password'),
         ]);
+
+        // Assigning a role to the user
+        $user->assignRole($userRole);
+
+        // Creating an admin user
+        $admin = User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        // Assigning the admin role to the user
+        $admin->assignRole($adminRole);
+
+        $agent = User::create([
+            'name' => 'Agent User',
+            'email' => 'agent@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        // Assigning the admin role to the user
+        $agent->assignRole($agentRole);
+
     }
 }
