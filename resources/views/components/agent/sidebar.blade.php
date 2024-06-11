7e1610c1e8834502723b70ba57935b23e3331cf6
bambangsy <bambangsy28@gmail.com>
2024-06-11 15:01:26 +0700

    fix header and footer

--- resources/views/components/agent/sidebar.blade.php
+++ resources/views/components/agent/sidebar.blade.php
@@ -2,9 +2,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ asset('AdminLTE-3.2.0/index3.html" class="brand-link') }}">
-        <img
-            src="{{ asset('AdminLTE-3.2.0/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8') }}">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <img src="{{ asset('image/logoeq.png')}}" alt="AdminLTE Logo" width="200px" class="mt-3 mx-auto d-block mb-4">
+        
     </a>
 
     <!-- Sidebar -->
@@ -13,7 +12,7 @@
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 <img
-                    src="{{ asset('AdminLTE-3.2.0/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image') }}">
+                    src="{{ asset('image/profil.png') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block">Alexander Pierce</a>
--- resources/views/layouts/agent.blade.php
+++ resources/views/layouts/agent.blade.php
@@ -46,10 +46,7 @@
   </div>
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
-    </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2024 <a href="">Earthqualizer</a>.</strong> All rights reserved.
   </footer>
 
   <!-- Control Sidebar -->
