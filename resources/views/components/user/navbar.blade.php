b0f065f2fdc97929db1ea081beb37bd11e855cff
Raka Dimas Saputra <rakadimas56@gmail.com>
2024-07-02 15:25:54 +0700

    fix register button in user view

--- resources/views/components/user/navbar.blade.php
+++ resources/views/components/user/navbar.blade.php
@@ -52,11 +52,11 @@
                                 <form method="POST" action="{{ route('logout') }}">
                                     @csrf
 
-                                    <button style="color: white; transition: color 0.3s;"
+                                    <a href="" onclick="event.preventDefault(); this.closest('form').submit();" style="color: white; transition: color 0.3s;"
                                         onmouseover="this.style.color='white'" onmouseout="this.style.color='white'"
                                         class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                                         Log Out
-                                    </button>
+                                    </a>
 
                                 </form>
                             @else
@@ -94,20 +94,41 @@
             </ul>
             <ul class="dx-nav dx-nav-align-right">
                 <li class="dx-nav-item">
+                    @auth
+                    <li class="dx-nav-item">
+                        <a href="" style="color: white; transition: color 0.3s;" onmouseover="this.style.color='grey'" onmouseout="this.style.color='white'">
+                            {{ Auth::user()->name }}
+                        </a>
+                    </li>
+                @endauth
+                </li>
+                
+                <li class="dx-nav-item">
                     @if (Route::has('login'))
                         <nav class="-mx-3 flex flex-1 justify-end">
                             @auth
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
-                                    <button class="dx-nav-link rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white" onmouseover="this.style.color='white'" onmouseout="this.style.color='white'">
-                                        Log Out
-                                    </button>
-                                </form>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <a href="" onclick="event.preventDefault(); this.closest('form').submit();" style="color: white; transition: color 0.3s;"
+                                    onmouseover="this.style.color='white'" onmouseout="this.style.color='white'"
+                                    class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                                    Log Out
+                                </a>
+
+                            </form>
                             @else
-                                <a href="{{ route('login') }}" class="dx-nav-link rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">Log in</a>
-                                @if (Route::has('register'))
-                                    <a href="{{ route('register') }}" class="dx-nav-link rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">Register</a>
-                                @endif
+                            <a href="{{ route('login') }}"
+                                class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                                Log in
+                            </a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}"
+                                    class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                                    Register
+                                </a>
+                            @endif
                             @endauth
                         </nav>
                     @endif
